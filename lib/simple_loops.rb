fcd34e6237badc38cdf2d316386f1bc9eaad68e9
Michael Lee <michael.l33.1994@gmail.com>
2020-09-21 19:18:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,9 +1,9 @@
 # Write your methods here
-def loop_message_five_times(message)
- counter = 0 
- while count < 5 do
-   puts message 
-   counter++
- end
+ddef loop_message_five_times(message)
+  count = 0
+  while count < 5 do
+    puts message
+    count += 1
+  end
 end
 
